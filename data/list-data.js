e5d9f6b1d9e65c1583c8fa7de7bea59ab94250e4
nl7912458-cmd <nl7912458@gmail.com>
2026-09-12 16:05:25 +0700

    Create list-data.js

---
+++ data/list-data.js
@@ -0,0 +1,20 @@
+// data/list-data.js
+export const listTaiLieu = [
+    {
+        id: 'tailieu1',
+        title: 'Chuyên đề: Con lắc lò xo & Lực đàn hồi',
+        description: 'Hệ thống công thức độc lập với thời gian và kỹ thuật tối ưu hóa.',
+        date: '12/09/2026'
+    },
+    {
+        id: 'tailieu2',
+        title: 'Ứng dụng vòng tròn lượng giác',
+        description: 'Kỹ thuật chiếu vectơ trên sơ đồ hình tròn trong dao động.',
+        date: '15/09/2026'
+    }
+];
+
+export const listDeThi = [
+    { id: 'de1', title: 'Đề thực chiến số 1', time: 60, questions: 50 },
+    // Thêm đề thi sau này ở đây
+];
